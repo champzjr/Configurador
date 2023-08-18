53e8554c01698b5c358fb2cf2085aad372563e46
oriondesign2015 <139019565+oriondesign2015@users.noreply.github.com>
2023-08-18 18:00:37 -0300

    Add files via upload

---
+++ Chatwoot.sh
@@ -0,0 +1,39 @@
+#########################################################
+
+read -p "Digite o dominio para acessar o Chatwoot (ex: app.dominio.com): " dominiochat
+echo ""
+read -p "Digite seu email de admin (ex: contato@dominio.com): " emailchat
+echo ""
+
+#########################################################
+
+clear
+
+sudo su
+
+cd
+
+echo "Atualizando a VPS"
+sudo apt update
+apt upgrade -y
+
+echo "Baixando install.sh do typebot"
+wget https://get.chatwoot.app/linux/install.sh
+
+echo "Dando permições ao arquivo"
+chmod +x install.sh
+
+echo "iniciando instalação"
+echo -e "yes\n$dominiochat\n$emailchat\nyes\nyes" | ./install.sh --install
+
+#########################################################
+
+#cd /home/chatwoot/chatwoot
+#
+#sudo mv .env .env-dev
+#
+#cd
+#
+#cd
+#
+#cat > .env << EOL
